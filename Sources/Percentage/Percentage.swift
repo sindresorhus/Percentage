86433d1b723437eb9eb91d7d971d9b45b3cd7f52
Sindre Sorhus <sindresorhus@gmail.com>
2020-04-30 22:57:23 +0800

    Update Percentage.swift

--- Sources/Percentage/Percentage.swift
+++ Sources/Percentage/Percentage.swift
@@ -107,17 +107,17 @@ public struct Percentage: Hashable, Codable {
 	*/
 	public func of(_ value: Double) -> Double { value * rawValue / 100 }
     
-    /**
-    Returns a random value within the specified range.
+	/**
+	Returns a random value within the given range.
 
-    ```
-    Percent.random(in: 10%...20%)
-    //=> can be 10%, 11%, 12%, 19.98%, etc.
-    ```
-     */
-    static func random(in range: ClosedRange<Percentage>) -> Percentage {
-        return Percentage(fraction: Double.random(in: range.lowerBound.fraction...range.upperBound.fraction))
-    }
+	```
+	Percent.random(in: 10%...20%)
+	//=> Can be 10%, 11%, 12%, 19.98%, etc.
+	```
+	*/
+	static func random(in range: ClosedRange<Percentage>) -> Percentage {
+		Percentage(fraction: Double.random(in: range.lowerBound.fraction...range.upperBound.fraction))
+	}
 }
 
 extension Percentage: RawRepresentable {
